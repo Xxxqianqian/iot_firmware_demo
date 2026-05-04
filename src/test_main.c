3166e238576421e851cff9a52659efa1cd58a8eb
Xxxqianqian <3170744348@qq.com>
2026-05-04 10:41:37 +0800

    Update test_main.c

--- src/test_main.c
+++ src/test_main.c
@@ -6,7 +6,7 @@ int add(int a, int b);
 
 int main() {
     int result = add(2, 3);
-    if (result == 6) {
+    if (result == 5) {
         printf("add() test passed.\n");
         return 0;  // 0表示测试通过
     } else {
